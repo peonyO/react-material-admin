8ed2458389f01382e8af0552fa572dc5884ad266
limuen <limuen_dev@163.com>
2024-03-14 15:38:06 +0800

    feat: 🚀 新增viteConfig相对应的plugins

--- src/services/requestHttp/index.ts
+++ src/services/requestHttp/index.ts
@@ -29,11 +29,10 @@ class RequestHttp {
      */
     this.service.interceptors.request.use(
       (config: CustomAxiosRequestConfig) => {
-        // 重复请求不需要取消，在 api 服务中通过指定的第三个参数: { cancel: false } 来控制
         config.cancel ?? (config.cancel = true);
         config.cancel && axiosCanceler.addPending(config);
-        // 当前请求不需要显示 loading，在 api 服务中通过指定的第三个参数: { loading: false } 来控制
         config.loading && showFullScreenLoading();
+
         if (config.headers && typeof config.headers.set === "function") {
           const tokenInfo = getStorage("tokenInfo");
           tokenInfo && config.headers.set("x-access-token", tokenInfo.token);
@@ -46,41 +45,38 @@ class RequestHttp {
     );
 
     /**
-     * @description response interceptor
-     *  The server returns the information -> [intercept unified processing] -> the client JS gets the information
+     * @description 响应拦截器
+     *  服务器换返回信息 -> [拦截统一处理] -> 客户端JS获取到信息
      */
     this.service.interceptors.response.use(
       (response: CustomAxiosResponse) => {
         const { data, config } = response;
         axiosCanceler.removePending(config);
         tryHideFullScreenLoading();
-        // login failure
+
         if (data.code == ResultEnum.OVERDUE) {
           removeStorage("tokenInfo");
           enqueueSnackbar({ variant: "error", message: "登录信息失效，请重新登录" });
           window.$navigate("/login");
           return Promise.reject(data);
         }
-        // Global error information interception (to prevent data stream from being returned when downloading files, and report errors directly without code)
+
         if (data.code && data.code !== ResultEnum.SUCCESS) {
           if (config.showDialog) {
             enqueueSnackbar({ variant: "error", message: data.message });
           }
           return Promise.reject(data);
         }
-        // Successful request (no need to handle failure logic on the page unless there are special circumstances)
+
         return data;
       },
       async (error: AxiosError) => {
         const { response } = error;
         tryHideFullScreenLoading();
-        // Request timeout && network error judged separately, no response
         if (error.message.indexOf("timeout") !== -1) enqueueSnackbar({ variant: "error", message: "请求超时！请您稍后重试" });
         if (error.message.indexOf("Network Error") !== -1)
           enqueueSnackbar({ variant: "error", message: "网络错误！请您稍后重试" });
-        // Do different processing according to the error status code of the server response
         if (response) checkStatus(response.status);
-        // The server does not return any results (maybe the server is wrong or the client is disconnected from the network), disconnection processing: you can jump to the disconnection page
         if (!window.navigator.onLine) window.$navigate("/500");
         return Promise.reject(error);
       }
---
+++ vitePlugs/getEnv.ts
@@ -0,0 +1,48 @@
+import path from "path";
+
+export function isDevFn(mode: string): boolean {
+  return mode === "development";
+}
+
+export function isProdFn(mode: string): boolean {
+  return mode === "production";
+}
+
+export function isTestFn(mode: string): boolean {
+  return mode === "test";
+}
+
+/**
+ * 是否生成包预览
+ */
+export function isReportMode(): boolean {
+  return process.env.VITE_REPORT === "true";
+}
+
+// 读取所有要处理的环境变量配置文件.env
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+  const ret: any = {};
+
+  for (const envName of Object.keys(envConf)) {
+    let realName = envConf[envName].replace(/\\n/g, "\n");
+    realName = realName === "true" ? true : realName === "false" ? false : realName;
+    if (envName === "VITE_PORT") realName = Number(realName);
+    if (envName === "VITE_PROXY_URL") {
+      try {
+        realName = JSON.parse(realName);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    ret[envName] = realName;
+  }
+  return ret;
+}
+
+/**
+ * 获取用户根目录
+ * @param dir file path
+ */
+export function getRootPath(...dir: string[]) {
+  return path.resolve(process.cwd(), ...dir);
+}
